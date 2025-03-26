44e958490c223638f8f63133953e0c2a8e44749e
gilsonsilven <gilsonsilven@gmail.com>
2025-03-25 22:16:32 -0300

    feat: adding fetch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-const loadProperties = () => {
-    const response = 
+const loadProperties = async () => {
+    const response = await fetch('http://localhost:3000/property/list')
+    
+    const data = await response.json()
 }
