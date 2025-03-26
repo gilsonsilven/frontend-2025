7d439ba59045fc15d78349e2c17f62af588b80d7
gilsonsilven <gilsonsilven@gmail.com>
2025-03-25 22:22:42 -0300

    asasasa

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@ const loadProperties = async () => {
     const response = await fetch('http://localhost:3000/property/list')
     
     const data = await response.json()
+
+    const divList = document.getElementById('property-list')
+
+    data.map((property) => {
+        divList.innerHTML += `
+        <div class="property-card">
+            <div class="icon-property">
+                <span>i-casa</span>
+            </div>
+            <div class="texts">
+                <h5>${property.type}</h5>
+                <p>${property.address}</p>
+                <p>${property.property}</p>
+            </div>
+        </div>
+        `
+    })
+    
 }
